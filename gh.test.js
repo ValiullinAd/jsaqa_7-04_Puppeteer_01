c451a6fafc2c3c4cb84453e776602c22b244558d
ValiullinAd <93069873+ValiullinAd@users.noreply.github.com>
2022-07-22 07:41:43 +0300

    Modify gh.test.js

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,61 +9,69 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
-    await page.setDefaultTimeout(50000);
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+  }, 50000);
 
   test("The first link attribute", async () => {
-    await page.setDefaultTimeout(50000);
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 50000);
 
   test("The page contains Sign in button", async () => {
-    await page.setDefaultTimeout(50000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
-  });
+  }, 50000);
 });
 
 describe("Github security page test", () => {
 
   beforeEach(async () => {
-    page = await browser.newPage();
     await page.goto("https://github.com/features/security");
   });
-
-  afterEach(() => {
-    page.close();
-  });
   
-  test("h1 security text", async () => {
-    await page.setDefaultTimeout(50000);
+  test("h1 support text", async () => {
     const h1 = "div div div h1";
     const h1Text = await page.$eval(h1, elem => elem.textContent);
     expect(h1Text).toEqual("Secure at every\u00A0step");
-  });
+  }, 50000);
 
   test("h4 test", async () => {
-    await page.setDefaultTimeout(50000);
     const h4Span = "div div div h4 span";
     const h4SpanText = await page.$eval(h4Span, elem => elem.textContent);
     expect(h4SpanText).toEqual("Ship secure applications within the GitHub flow");
-  });
+  }, 50000);
 
   test("Button text test", async () => {
-    await page.setDefaultTimeout(50000);
     const headerEl = "div main div div a";
     const elementText = await page.$eval(headerEl, elem => elem.textContent);
     expect(elementText).toEqual("Security");
- });
+ }, 50000);
+});
+
+describe("Github support page test", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://support.github.com/");
+  });
+  
+   test("h3 page test", async () => {
+    const h3Text = "div h3";
+    const h3ElText = await page.$eval(h3Text, elem => elem.textContent);
+    expect(h3ElText).toEqual("Virtual Assistant");
+  }, 50000);
+
 });
